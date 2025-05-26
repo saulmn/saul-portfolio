d3f53a32edf1c273c10fc05247b6962d63c3f599
Saul Maldonado Navarro <saulmn.2599@gmail.com>
2025-05-25 19:09:05 -0600

    update: Update Tailwind CSS to V4.1

--- src/components/NavBar/navbar.tsx
+++ src/components/NavBar/navbar.tsx
@@ -2,7 +2,7 @@ import { Logo } from '../Logo/logo'
 
 export function NavBar() {
 	return (
-		<nav className="sticky top-0 z-20 flex h-14 w-full items-center justify-between bg-white bg-opacity-30 backdrop-blur-lg shadow-sm p-5 text-black xl:px-10">
+		<nav className="sticky top-0 z-20 flex h-14 w-full items-center justify-between bg-white bg-opacity-30 backdrop-blur-lg shadow-xs p-5 text-black xl:px-10">
 			<div className="flex items-center space-x-6">
 				<Logo />
 			</div>
