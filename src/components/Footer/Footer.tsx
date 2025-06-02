b5103053e592adc54eb95f649f973c8b1dd6edf6
Saul Maldonado Navarro <saulmn.2599@gmail.com>
2025-06-02 14:29:02 -0600

    feat: add new icons and improve experience section
    - Added new SVG icons for CV, GitHub, LinkedIn, and Mail.
    - Updated package.json to include @radix-ui/react-tooltip and @tabler/icons-react.
    - Refactored Experience component to use a timeline layout with detailed entries.
    - Updated Footer component to include a heart emoji.
    - Enhanced Hero component with animated link icons for GitHub, LinkedIn, CV, and Email.
    - Created a new Timeline component for better presentation of work experience.
    - Updated sly.json to change icon directory to public/icons.
    - Updated index.astro to load Hero and Experience components on the client side.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <section className="flex flex-col justify-center items-center p-4">
-      <p>Made with in TLX</p>
+      <p>Made with ❤️ in TLX</p>
       <p>Saul Maldonado Navarro.</p>
     </section>
   );
--- src/components/Hero/hero.tsx
+++ src/components/Hero/hero.tsx
@@ -1,47 +1,63 @@
-// Components
-import { Button } from '../ui/button'
+import { motion } from "motion/react";
 
 export function Hero() {
-	return (
-		<section className="w-screen h-screen flex justify-center items-center p-4">
-			<div className="grid grid-cols-1 md:grid-cols-2 max-w-5xl gap-10">
-				<div className="flex flex-col items-start justify-center gap-10">
-					<h1 className="text-5xl font-semibold text-left">
-						Hi ✌️, my name is Saúl and i'm a Computer Science Engineer.
-					</h1>
-					<div className="flex justify-between w-full gap-10">
-						<a
-							href="https://www.github.com/saulmn"
-							target="_blank"
-							rel="noreferrer"
-						>
-							Github
-						</a>
-						<a
-							href="https://www.linkedin.com/in/saulmn/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							in
-						</a>
+  const links = [
+    {
+      href: "https://www.github.com/saulmn",
+      icon: "/icons/github.svg",
+      alt: "GitHub",
+    },
+    {
+      href: "https://www.linkedin.com/in/saulmn/",
+      icon: "/icons/linkedin.svg",
+      alt: "Linkedin",
+    },
+    {
+      href: "https://www.drive.com/",
+      icon: "/icons/cv.svg",
+      alt: "CV",
+    },
+    {
+      href: "mailto:saulmn.2599@gmail.com",
+      icon: "/icons/mail.svg",
+      alt: "Email",
+    },
+  ];
 
-						<a href="https://www.drive.com/" target="_blank" rel="noreferrer">
-							CV
-						</a>
-						<a href="https://www.drive.com/" target="_blank" rel="noreferrer">
-							Mail
-						</a>
-					</div>
-				</div>
-				<div className="flex justify-center items-center max-w-3xl">
-					<img
-						alt="Hi!"
-						loading="lazy"
-						className="rounded-full"
-						src="https://michaeldeboey.be/img/Micha%C3%ABl.jpg"
-					/>
-				</div>
-			</div>
-		</section>
-	)
+  return (
+    <section className="w-screen h-screen flex justify-center items-center p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 max-w-5xl gap-10">
+        <div className="flex flex-col items-start justify-center gap-10">
+          <h1 className="text-4xl font-semibold text-left">
+            Hi ✌️, my name is Saúl and I'm a Computer Science Engineer.
+          </h1>
+          <div className="flex justify-around w-full">
+            {links.map((link, index) => (
+              <motion.a
+                key={index}
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 0.8 }}
+                href={link.href}
+                aria-label={link.alt}
+                title={link.alt}
+                target="_blank"
+                rel="noreferrer"
+                className="hover:bg-gray-200 p-3 rounded-full transition-colors duration-300"
+              >
+                <img src={link.icon} alt={link.alt} className="w-6 h-6" />
+              </motion.a>
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-center items-center max-w-3xl">
+          <img
+            alt="Hi!"
+            loading="lazy"
+            className="rounded-full"
+            src="https://michaeldeboey.be/img/Micha%C3%ABl.jpg"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
