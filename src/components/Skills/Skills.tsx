8e1223916b9a84f50c8f212e227482eec0b68d92
Saul Maldonado Navarro <saulmn.2599@gmail.com>
2025-06-10 19:44:18 -0600

    feat: add FastAPI icon and enhance Technologies component with new categories for languages, frameworks, tools, and DevOps

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,7 +1,7 @@
-import { WobbleCard } from "../ui/wobble-card";
 import { cn } from "@/lib/utils";
-import { technologies } from "@/constants/technologies";
 import { Separator } from "../ui/separator";
+import { WobbleCard } from "../ui/wobble-card";
+import { tools, devops, languages, frameworks } from "@/constants/technologies";
 
 export function Technologies() {
   return (
@@ -14,26 +14,101 @@ export function Technologies() {
         <p className="mt-2 text-neutral-700">
           A list of languages, frameworks, and tools I’ve worked with recently.
         </p>
-        <div className="mt-10 flex items-center justify-center">
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-            {technologies.map((tech) => (
-              <WobbleCard
-                key={tech.name}
-                className={`p-4 flex flex-col items-center justify-center`}
-                containerClassName={cn(
-                  `hover:opacity-90 rounded-full transition-all duration-300 ease-in-out`,
-                  tech.color
-                )}
-              >
-                <img
+        <div className="mt-10 flex flex-col">
+          <div className="flex flex-col items-end justify-end max-w-5xl">
+            <h3 className="text-2xl">Languages</h3>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+              {languages.map((tech) => (
+                <WobbleCard
                   key={tech.name}
-                  src={tech.icon}
-                  alt={tech.name}
-                  className="w-6 h-6"
-                />
-                <span className="text-white mt-2">{tech.name}</span>
-              </WobbleCard>
-            ))}
+                  className={`p-4 flex flex-col items-center justify-center`}
+                  containerClassName={cn(
+                    `hover:opacity-90 rounded-full transition-all duration-300 ease-in-out`,
+                    tech.color
+                  )}
+                >
+                  <img
+                    key={tech.name}
+                    src={tech.icon}
+                    alt={tech.name}
+                    className="w-8 h-8"
+                  />
+                  <span className="text-white mt-2">{tech.name}</span>
+                </WobbleCard>
+              ))}
+            </div>
+            <Separator className="my-10" />
+          </div>
+          <div className="flex flex-col items-start justify-start max-w-5xl">
+            <h3 className="text-2xl">Frameworks</h3>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+              {frameworks.map((tech) => (
+                <WobbleCard
+                  key={tech.name}
+                  className={`p-4 flex flex-col items-center justify-center`}
+                  containerClassName={cn(
+                    `hover:opacity-90 rounded-full transition-all duration-300 ease-in-out`,
+                    tech.color
+                  )}
+                >
+                  <img
+                    key={tech.name}
+                    src={tech.icon}
+                    alt={tech.name}
+                    className="w-8 h-8"
+                  />
+                  <span className="text-white mt-2">{tech.name}</span>
+                </WobbleCard>
+              ))}
+            </div>
+            <Separator className="my-10" />
+          </div>
+          <div className="flex flex-col items-end justify-end max-w-5xl">
+            <h3 className="text-2xl">Tools</h3>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+              {tools.map((tech) => (
+                <WobbleCard
+                  key={tech.name}
+                  className={`p-4 flex flex-col items-center justify-center`}
+                  containerClassName={cn(
+                    `hover:opacity-90 rounded-full transition-all duration-300 ease-in-out`,
+                    tech.color
+                  )}
+                >
+                  <img
+                    key={tech.name}
+                    src={tech.icon}
+                    alt={tech.name}
+                    className="w-8 h-8"
+                  />
+                  <span className="text-white mt-2">{tech.name}</span>
+                </WobbleCard>
+              ))}
+            </div>
+            <Separator className="my-10" />
+          </div>
+          <div className="flex flex-col items-start justify-start max-w-5xl">
+            <h3 className="text-2xl">DevOps</h3>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+              {devops.map((tech) => (
+                <WobbleCard
+                  key={tech.name}
+                  className={`p-4 flex flex-col items-center justify-center`}
+                  containerClassName={cn(
+                    `hover:opacity-90 rounded-full transition-all duration-300 ease-in-out`,
+                    tech.color
+                  )}
+                >
+                  <img
+                    key={tech.name}
+                    src={tech.icon}
+                    alt={tech.name}
+                    className="w-8 h-8"
+                  />
+                  <span className="text-white mt-2">{tech.name}</span>
+                </WobbleCard>
+              ))}
+            </div>
           </div>
         </div>
       </div>
